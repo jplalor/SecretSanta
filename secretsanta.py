4aef5b09da6dc097b6867d0f5467cd71b36acbf3
John Lalor <jlalor123@gmail.com>
2012-11-27 16:04:14 -0600

    Updated Version-Fixed alot

--- secretsanta.py
+++ secretsanta.py
@@ -1,13 +1,19 @@
 #THIS CURRENT CONFIG WORKSSSS
 
+import cgi
 import webapp2
 #from jinja2 import Environment, PackageLoader
 import jinja2
 
-#from google.appengine.ext import db
+from google.appengine.ext import db
 
 env = jinja2.Environment(autoescape=True,
 	loader=jinja2.FileSystemLoader('templates'))
+	
+class Person (db.Model):
+	name = db.StringProperty()
+	email=db.StringProperty()
+	date=db.DateTimeProperty(auto_now_add=True)
 
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
@@ -21,9 +27,19 @@ class Handler(webapp2.RequestHandler):
 class MainPage(Handler):
     def get(self):
         template = env.get_template('front.html')
-        #posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
-        #self.response.out.write(template.render(posts))
-        self.render(template, huge = 'Captain Huuuuuugggggeeee!!!')
+        self.render(template)
+		
+class ThankYou(Handler):
+	def post(self):
+		fullname = self.request.get('fullname')
+		emailaddress=self.request.get('emailaddress')
+		p = Person(name=fullname, email=emailaddress)
+		p.put()
+		people = db.GqlQuery("SELECT * "
+							 "FROM Person "
+							 "ORDER BY date DESC")
+		template=env.get_template('thanks.html')
+		self.response.out.write(template.render({'people': people}))
 
 
 #class Handler(webapp2.RequestHandler):
@@ -44,5 +60,6 @@ class MainPage(Handler):
         #self.response.out.write(template)
 
 app = webapp2.WSGIApplication([('/', MainPage),
+								('/thanks.html', ThankYou),
 	],
 	debug=True)
